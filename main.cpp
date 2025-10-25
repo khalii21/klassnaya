8ae2be06b3a0d6d957a22bea6f2b7cc8767f4220
khalii21 <miratkhalikov007@gmail.com>
2025-10-25 17:06:23 +0300

    added overflow check

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 bool isPyth(unsigned a, unsigned b, unsigned c)
 {
 bool p = a*a==b*b+c*c;
@@ -6,15 +7,28 @@ p = p || (b*b==a*a+c*c);
 p = p || (c*c==a*a+b*b);
 return p;
 }
+unsigned max_u()
+{
+	using namespace std;
+	using u_limit = numeric_limits< unsigned >;
+	return u_limit::max();
+}
+bool perepolnenie(unsigned a, unsigned b, unsigned c)
+{
+bool p = a*a > max_u() - b*b;
+p = p || b*b > max_u() - c*c;
+p = p || a*a > max_u() - c*c;
+return p;
+}
 int main()
 {
 	using u_t = unsigned;
-	u_t a = 0; 
-	u_t b =0; 
+	u_t a = 0;
+	u_t b =0;
 	u_t c = 0;
 	std::cin >> c >> b;
 	size_t count = 0;
-	while (std::cin >> a) 
+	while (std::cin >> a)
 		{
 		count+=isPyth(a,b,c) ? 1:0;
 		c = b;
@@ -25,10 +39,14 @@ int main()
 		std::cout << count;
 		std::cout << "\n";
 		}
-
-	else if (std::cin.fail()) 
+	else if (perepolnenie(a,b,c))
 		{
-		std::cerr << "not a sequency";
+		std::cerr << "overflow\n";
+		return 2;
+		}
+	else if (std::cin.fail())
+		{
+		std::cerr << "not a sequency\n";
 		return 1;
-		} 
+		}
 }
