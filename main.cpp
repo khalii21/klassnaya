f840d46191637b791d2cc9e75fa570acbd50ecbf
khalii21 <miratkhalikov007@gmail.com>
2025-10-25 14:37:59 +0300

    count of seq

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
+#include <iostream>
 int main()
 {
 	using u_t = unsigned;
 	u_t a = 0;
-	std::cin >> a;
+	size_t count = 0;
+	while (std::cin >> a) 
+		{
+		count++;
+		}
+	if (std::cin.eof())
+		{
+		std::cout << count;
+		std::cout << "\n";
+		}
+
+	else if (std::cin.fail()) 
+		{
+		std::cerr << "not a sequency";
+		return 1;
+		} 
 }
